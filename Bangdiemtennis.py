493bde9fe0a6be82c5c466c2256e91a753ed7c79
nhanntk25406-uel <nhanntk25406@st.uel.edu.vn>
2025-11-01 17:48:33 +0700

    Notyet

--- Bangdiemtennis.py
+++ Bangdiemtennis.py
@@ -2,7 +2,7 @@ import sys
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QApplication, QWidget
 from giaodienbangdiemtennis import Ui_bangdiemtennis
-from xuly import reset_game, get_display_score, check_winner, update_score, player_A_score
+import xuly
 
 app = QApplication(sys.argv)
 window = QWidget(windowTitle = 'Quy doi he')
@@ -10,19 +10,23 @@ form =Ui_bangdiemtennis()
 form.setupUi(window)
 
 
-INdiem1=["0","15","30","40"]
-INdiem2=["0","15","30","40"]
+
 def player_A():
-    global player_A_score
-    player_A_score+=1
-    form.INdiem1.setText(INdiem1[(player_A_score)])
+    xuly.player_A_score+=1
+    form.INdiem1.setText(str(xuly.score_names[(xuly.player_A_score)]))
+
+def player_B():
+    xuly.player_B_score+=1
+    form.INdiem1.setText(str(xuly.score_names[(xuly.player_B_score)]))
+
 
 def reset():
-    reset_game()
-    form.linEketqua.setText(reset_game())
+    form.INdiem1.setText("0")
+    form.INdiem1_2.setText("0")
+    form.linEketqua.setText(xuly.reset_game())
 
 form.bt1.clicked.connect(player_A)
-form.bt2.clicked.connect(player_A)
+form.bt2.clicked.connect(player_B)
 form.btreset.clicked.connect(reset)
 
 
